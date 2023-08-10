a2fcafcb9058a189753caf1c7a6e5ea4a37b64a4
samiulalimsaad <samiulalimsaad@gmail.com>
2023-08-11 00:49:10 +0600

    all queries to create tables

--- all created.sql
+++ all created.sql
@@ -1,3 +1,26 @@
+-- delete all databases
+DROP TABLE IF EXISTS teaches;
+
+DROP TABLE IF EXISTS advisor;
+
+DROP TABLE IF EXISTS instructor;
+
+DROP TABLE IF EXISTS takes;
+
+DROP TABLE IF EXISTS student;
+
+DROP TABLE IF EXISTS section;
+
+DROP TABLE IF EXISTS time_slot;
+
+DROP TABLE IF EXISTS prereq;
+
+DROP TABLE IF EXISTS course;
+
+DROP TABLE IF EXISTS classroom;
+
+DROP TABLE IF EXISTS department;
+
 -- Create the "department" table
 CREATE TABLE
     department (
